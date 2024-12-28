20544dc82f10f9f826e28e9a337fb9fb69b08996
Carlos Hernandez <carlosandresh925@gmail.com>
2024-12-28 11:20:05 -0800

    began problem 2

---
+++ Java/AddTwoNum_2.java
@@ -0,0 +1,19 @@
+public class AddTwoNum_2 {
+    static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        
+
+        return new ListNode();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("#2 - Add Two Numbers - Medium");
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) {this.val = val; this.next = next;}
+}
