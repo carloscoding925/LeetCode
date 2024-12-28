41a6182316e8e6d41807937cbebc30930a5560d5
Carlos Hernandez <carlosandresh925@gmail.com>
2024-12-27 20:31:23 -0800

    twosum solution

--- Java/TwoSum_1.java
+++ Java/TwoSum_1.java
@@ -2,15 +2,15 @@ public class TwoSum_1{
     static int[] twoSum(int[] nums, int target) {
         int[] indices = new int[2];
 
-        for (int i = 0; i < nums.length; i++){
-            for (int j = 0; j < nums.length; i++) {
-                if (nums[i] + nums[j] == target) {
+        for (int i = 0; i < nums.length; i++) {
+            int currentNumber = nums[i];
+            int complement = target - currentNumber;
+
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == complement) {
                     indices[0] = i;
                     indices[1] = j;
                     return indices;
-                }
-                else {
-                    continue;
                 }
             }
         }
@@ -21,7 +21,7 @@ public class TwoSum_1{
     public static void main(String[] args) {
         System.out.println("#1 - Two Sum - Easy");
 
-        int[] nums = {3,2,4};
+        int[] nums = {3,3};
         int target = 6;
         int[] result = twoSum(nums, target);
 
