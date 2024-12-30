caadc3cb8a12f5a69b2740b8f121ddd382ceecd0
Carlos Hernandez <carlosandresh925@gmail.com>
2024-12-29 17:07:44 -0800

    leetcode 9 - palindrome number

---
+++ Java/PalindromeNum_9.java
@@ -0,0 +1,22 @@
+public class PalindromeNum_9 {
+    static boolean isPalindrome(int x) {
+        String integerString = Integer.toString(x);
+        int length = integerString.length();
+        int midPoint = length / 2;
+
+        for (int i = 0; i < midPoint; i++) {
+            if (integerString.charAt(i) != integerString.charAt(length - i - 1)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("#9 - Palindrome Number - Easy");
+        boolean result = isPalindrome(125521);
+
+        System.out.println("Is Palindrome: " + result);
+    }
+}
