7ffc8434487f986ba81fe9019f80320b3fd71052
Carlos Hernandez <carlosandresh925@gmail.com>
2025-02-06 13:49:02 -0800

    problem 2 solved? - gonna need to study lol

--- Java/Medium/AddTwoNum_2.java
+++ Java/Medium/AddTwoNum_2.java
@@ -1,19 +1,49 @@
 public class AddTwoNum_2 {
-    static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        
+    // Given ListNode Class
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
 
-        return new ListNode();
+    static ListNode solutionOne(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode();
+        ListNode tail = head;
+        int carry = 0;
+
+        while(l1 != null || l2 != null || carry != 0) {
+            int numberOne = l1 != null ? l1.val : 0;
+            int numberTwo = l2 != null ? l2.val : 0;
+
+            int sum = numberOne + numberTwo + carry;
+            int value = sum % 10;
+            carry = sum / 10;
+
+            ListNode nextNode = new ListNode(value);
+            tail.next = nextNode;
+            tail = tail.next;
+
+            l1 = l1 != null ? l1.next : null;
+            l2 = l2 != null ? l2.next : null;
+        }
+
+        ListNode result = head.next;
+        head.next = null;
+
+        return result;
     }
 
     public static void main(String[] args) {
         System.out.println("#2 - Add Two Numbers - Medium");
-    }
-}
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+        ListNode result = solutionOne(l1, l2);
 
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) {this.val = val; this.next = next;}
+        while(result != null) {
+            System.out.println(result.val);
+            result = result.next;
+        }
+    }
 }
