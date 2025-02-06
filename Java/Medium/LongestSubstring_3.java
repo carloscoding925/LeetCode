d5b6d35d37ab06da8d03a68e25ec031a95581ee9
Carlos Hernandez <carlosandresh925@gmail.com>
2025-02-06 14:12:36 -0800

    saving

---
+++ Java/Medium/LongestSubstring_3.java
@@ -0,0 +1,9 @@
+public class LongestSubstring_3 {
+    static int solutionOne(String s) {
+        
+        return 0;
+    }
+    public static void main(String[] args) {
+        System.out.println("#3 - Longest Substring Without Repeating Characters - Medium");
+    }
+}
