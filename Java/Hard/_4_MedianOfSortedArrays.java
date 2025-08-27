14dbb17926e162a472b7d9d54e9612171f65f055
Carlos Hernandez <carlosandresh925@gmail.com>
2025-08-26 20:28:06 -0700

    Wtf

--- Java/Hard/_4_MedianOfSortedArrays.java
+++ Java/Hard/_4_MedianOfSortedArrays.java
@@ -15,48 +15,6 @@ public class _4_MedianOfSortedArrays {
     }
 
     private static double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int length = nums1.length + nums2.length;
-        boolean isEven = length % 2 == 0;
-        boolean isFirstArray = false;
-
-        int firstPointer = 0;
-        int secondPointer = 0;
-
-        for (int i = 0; i < length / 2; i++) {
-            int firstVal = nums1[firstPointer];
-            int secondVal = nums2[secondPointer];
-
-            if (firstVal > secondVal) {
-                isFirstArray = true;
-                secondPointer++;
-            }
-            else if (secondVal > firstVal) {
-                isFirstArray = false;
-                firstPointer++;
-            }
-            else {
-                firstPointer++;
-                secondPointer++;
-            }
-        }
-
-        int median;
-        if (isFirstArray) {
-            median = nums1[firstPointer];
-        }
-        else {
-            median = nums2[secondPointer];
-        }
-
-        if (isEven) {
-            if (isFirstArray) {
-                median = nums1[firstPointer];
-            }
-            else {
-                median = nums2[secondPointer];
-            }
-        }
-
-        return median;
+        return 0.0;
     }
 }
