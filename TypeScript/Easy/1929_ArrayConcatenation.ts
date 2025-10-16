100dcb3d08f4a5a19ac60ec5e5f3d7a746fc5ee2
Carlos Hernandez <carlosandresh925@gmail.com>
2025-10-15 20:17:45 -0700

    LeetCode 1929 - TS

--- TypeScript/Easy/1929_ArrayConcatenation.ts
+++ TypeScript/Easy/1929_ArrayConcatenation.ts
@@ -1,2 +1,21 @@
-let variable: string = "doot";
-console.log(variable);
+function getConcatenation(nums: number[]): number[] {
+    let length: number = nums.length;
+    let concatenated: number[] = [];
+
+    for (let i = 0; i < length; i++) {
+        concatenated[i] = nums[i];
+        concatenated[nums.length + i] = nums[i];
+    }
+
+    return concatenated;
+
+    // Alternative Solutions
+
+    // return nums.concat(nums);
+    // return [...nums, ...nums];
+}
+
+let testArray: number[] = [1, 2, 3];
+let result = getConcatenation(testArray);
+console.log("#1929 - Concatenation of Array - Easy");
+console.log(result);
