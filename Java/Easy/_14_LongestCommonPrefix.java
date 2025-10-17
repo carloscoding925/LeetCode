60da8e88f18697333fb07343d0156bb9c2d2b2c6
Carlos Hernandez <carlosandresh925@gmail.com>
2025-10-16 22:28:53 -0700

    LeetCode 14 LCP Java

---
+++ Java/Easy/_14_LongestCommonPrefix.java
@@ -0,0 +1,42 @@
+package Java.Easy;
+
+public class _14_LongestCommonPrefix {
+    public static void main(String[] args) {
+        System.out.println("#14 - Longest Common Prefix - Easy");
+
+        String[] stringArray = {"flower", "flow", "flight"};
+        String lcp = longestCommonPrefix(stringArray);
+        System.out.println("Longest Common Prefix is: " + lcp);
+
+        return;
+    }
+
+    private static String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+
+        String lcp = strs[0];
+
+        for (String nextString : strs) {
+            int lcpLength = lcp.length();
+            int nextStringLength = nextString.length();
+            int iterator = 0;
+            StringBuilder temp = new StringBuilder();
+
+            while (iterator < lcpLength && iterator < nextStringLength) {
+                if (lcp.charAt(iterator) == nextString.charAt(iterator)) {
+                    temp.append(lcp.charAt(iterator));
+                    iterator++;
+                }
+                else {
+                    break;
+                }
+            }
+
+            lcp = temp.toString();
+        }
+
+        return lcp;
+    }
+}
