c2a14f9edb7801847e219935c09592b8a94fb7b4
Carlos Hernandez <carlosandresh925@gmail.com>
2025-10-18 18:10:49 -0700

    LeetCode 49 Java correct solution

--- Java/Medium/_49_GroupAnagrams.java
+++ Java/Medium/_49_GroupAnagrams.java
@@ -1,6 +1,7 @@
 package Java.Medium;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,71 +19,24 @@ public class _49_GroupAnagrams {
     }
 
     private static List<List<String>> groupAnagrams(String[] strs) {
-        if (strs.length == 0) {
-            return List.of();
-        }
-
-        List<List<String>> anagramList = new ArrayList<>();
+        Map<String, List<String>> anagramsMap = new HashMap<>();
 
         for (String nextString : strs) {
-            if (anagramList.isEmpty()) {
-                anagramList.add(new ArrayList<>(List.of(nextString)));
-                continue;
+            char[] charArray = nextString.toCharArray();
+            Arrays.sort(charArray);
+            String sortedString = new String(charArray);
+
+            if (anagramsMap.containsKey(sortedString)) {
+                List<String> anagramsList = anagramsMap.get(sortedString);
+                anagramsList.add(nextString);
+                anagramsMap.put(sortedString, anagramsList);
             }
-
-            Map<Character, Integer> nextStringMap = new HashMap<>();
-            int i = 0;
-
-            while (i < nextString.length()) {
-                if (nextStringMap.containsKey(nextString.charAt(i))) {
-                    nextStringMap.put(nextString.charAt(i), nextStringMap.get(nextString.charAt(i)) + 1);
-                }
-                else {
-                    nextStringMap.put(nextString.charAt(i), 1);
-                }
-                i++;
-            }
-
-            boolean isAnagram = false;
-
-            for (int k = 0; k < anagramList.size(); k++) {
-                if (isAnagram) { continue; }
-
-                Map<Character, Integer> copyMap = new HashMap<>(nextStringMap);
-                List<String> anagrams = anagramList.get(k);
-                String existingAnagram = anagrams.get(0);
-                int j = 0;
-
-                while (j < existingAnagram.length()) {
-                    if (copyMap.containsKey(existingAnagram.charAt(j))) {
-                        copyMap.put(existingAnagram.charAt(j), copyMap.get(existingAnagram.charAt(j)) - 1);
-
-                        if (copyMap.get(existingAnagram.charAt(j)) == 0) {
-                            copyMap.remove(existingAnagram.charAt(j));
-                        }
-                    }
-                    else {
-                        break;
-                    }
-                    j++;
-                }
-
-                if (copyMap.isEmpty() && nextString.length() != 0) {
-                    isAnagram = true;
-                    anagramList.get(k).addLast(nextString);
-                }
-
-                if (copyMap.isEmpty() && existingAnagram.length() == 0) {
-                    isAnagram = true;
-                    anagramList.get(k).addLast(nextString);
-                }
-            }
-
-            if (!isAnagram) {
-                anagramList.add(new ArrayList<>(List.of(nextString)));
+            else {
+                List<String> newList = new ArrayList<>(List.of(nextString));
+                anagramsMap.put(sortedString, newList);
             }
         }
 
-        return anagramList;
+        return new ArrayList<>(anagramsMap.values());
     }
 }
--- WIP.md
+++ WIP.md
@@ -1,4 +1,3 @@
 ### WIP
 
 - #4 Median of Two Sorted Arrays - Hard -- Java
-- #49 Group Anagrams - Medium -- Java
