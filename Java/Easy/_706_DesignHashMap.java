97cd7c06c02717d6321b38e2cd6a699ff6b3ba8b
Carlos Hernandez <carlosandresh925@gmail.com>
2025-10-21 22:17:56 -0700

    LeetCode 706 chaining method

--- Java/Easy/_706_DesignHashMap.java
+++ Java/Easy/_706_DesignHashMap.java
@@ -16,6 +16,17 @@ public class _706_DesignHashMap {
         myHashMap.remove(2);
         System.out.println(myHashMap.get(2));
 
+        System.out.println("\n --- Testing with Chaining Method ---");
+        SeparateChainingMap chainingMap = new SeparateChainingMap();
+        chainingMap.put(1, 1);
+        chainingMap.put(2, 2);
+        System.out.println(chainingMap.get(1));
+        System.out.println(chainingMap.get(3));
+        chainingMap.put(2, 1);
+        System.out.println(chainingMap.get(2));
+        chainingMap.remove(2);
+        System.out.println(chainingMap.get(2));
+
         return;
     }
 
@@ -37,6 +48,93 @@ public class _706_DesignHashMap {
 
         public void remove(int key) {
             this.lookupArray[key] = -1;
+        }
+    }
+
+    static class SeparateChainingMap {
+        private static final int SIZE = 1000;
+        private Node[] buckets;
+
+        static class Node {
+            int key;
+            int value;
+            Node next;
+
+            Node(int key, int value) {
+                this.key = key;
+                this.value = value;
+                this.next = null;
+            }
+        }
+
+        public SeparateChainingMap() {
+            this.buckets = new Node[SIZE];
+        }
+
+        private int hash(int key) {
+            return key % SIZE;
+        }
+
+        public void put(int key, int value) {
+            int index = hash(key);
+
+            if (this.buckets[index] == null) {
+                this.buckets[index] = new Node(key, value);
+                return;
+            }
+
+            Node current = this.buckets[index];
+            while (true) {
+                if (current.key == key) {
+                    current.value = value;
+                    return;
+                }
+
+                if (current.next == null) {
+                    current.next = new Node(key, value);
+                    return;
+                }
+
+                current = current.next;
+            }
+        }
+
+        public int get(int key) {
+            int index = hash(key);
+            Node current = this.buckets[index];
+
+            while (current != null) {
+                if (current.key == key) {
+                    return current.value;
+                }
+
+                current = current.next;
+            }
+
+            return -1;
+        }
+
+        public void remove(int key) {
+            int index = hash(key);
+            Node current = this.buckets[index];
+
+            if (current == null) {
+                return;
+            }
+
+            if (current.key == key) {
+                this.buckets[index] = current.next;
+                return;
+            }
+
+            while (current.next != null) {
+                if (current.next.key == key) {
+                    current.next = current.next.next;
+                    return;
+                }
+
+                current = current.next;
+            }
         }
     }
 }
