7cfc5e9a97a13cb68b57bf5a106945bac35e8f25
Carlos Hernandez <carlosandresh925@gmail.com>
2025-10-23 18:44:20 -0700

    LeetCode 344 - Java

---
+++ Java/Easy/_344_ReverseString.java
@@ -0,0 +1,51 @@
+package Java.Easy;
+
+public class _344_ReverseString {
+    public static void main(String[] args) {
+        System.out.println("#344 - Reverse String - Easy");
+
+        char[] input = {'h', 'e', 'l', 'l', 'o'};
+        reverseString(input);
+        String inputString = new String(input);
+
+        System.out.println("Reversed String: " + inputString);
+
+        alternative(input);
+        String altString = new String(input);
+
+        System.out.println("Alternative Method: " + altString);
+
+        return;
+    }
+
+    private static void reverseString(char[] s) {
+        int lp = 0;
+        int rp = s.length - 1;
+
+        for (int i = 0; i < s.length / 2; i++) {
+            char lpChar = s[lp];
+            char rpChar = s[rp];
+
+            s[lp] = rpChar;
+            s[rp] = lpChar;
+
+            lp++;
+            rp--;
+        }
+    }
+
+    private static void alternative(char[] s) {
+        int lp = 0;
+        int rp = s.length - 1;
+
+        while (lp < rp) {
+            char temp = s[lp];
+
+            s[lp] = s[rp];
+            s[rp] = temp;
+
+            lp++;
+            rp--;
+        }
+    }
+}
