f5ab311db686ad3a672bfc47a9843ee9fe9658ac
Carlos Hernandez <carlosandresh925@gmail.com>
2025-10-30 21:19:22 -0700

    Saving progress

---
+++ Java/Easy/_543_DiameterOfTree.java
@@ -0,0 +1,27 @@
+package Java.Easy;
+
+public class _543_DiameterOfTree {
+    public static void main(String[] args) {
+        System.out.println("#543 - Diameter of Binary Tree - Easy");
+
+        return;
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private static int diameterOfBinaryTree(TreeNode root) {
+        return 0;
+    }
+}
--- WIP.md
+++ WIP.md
@@ -1,3 +1,4 @@
 ### WIP
 
 - #4 Median of Two Sorted Arrays - Hard -- Java
+- #543 Diameter of Binary Tree - Easy -- Java
