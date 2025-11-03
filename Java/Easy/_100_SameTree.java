a45b438958764017fe9575927e790ec0cb0c1604
Carlos Hernandez <carlosandresh925@gmail.com>
2025-11-02 19:08:52 -0800

    LeetCode 100 - Same Tree

---
+++ Java/Easy/_100_SameTree.java
@@ -0,0 +1,53 @@
+package Java.Easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _100_SameTree {
+    public static void main(String[] args) {
+        System.out.println("#100 - Same Tree - Easy");
+
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+
+        TreeNode root2 = new TreeNode(1);
+        root2.left = new TreeNode(2);
+        root2.right = new TreeNode(3);
+
+        boolean isEqual = isSameTree(root, root2);
+        System.out.println("Are Trees Equal: " + isEqual);
+
+        return;
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
