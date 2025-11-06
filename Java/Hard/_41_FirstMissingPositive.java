de5b14a87a25ad6032431941b189f4b72dee8532
Carlos Hernandez <carlosandresh925@gmail.com>
2025-11-05 19:26:02 -0800

    LeetCode 41 - First Missing Positive

--- Java/Hard/_41_FirstMissingPositive.java
+++ Java/Hard/_41_FirstMissingPositive.java
@@ -13,6 +13,27 @@ public class _41_FirstMissingPositive {
     }
 
     private static int firstMissingPositive(int[] nums) {
-        
+        int length = nums.length;
+
+        for (int i = 0; i < length; i++) {
+            if (nums[i] <= 0 || nums[i] > length) {
+                nums[i] = length + 1;
+            }
+        }
+
+        for (int i = 0; i < length; i++) {
+            int val = Math.abs(nums[i]);
+            if (val <= length) {
+                nums[val - 1] = -Math.abs(nums[val - 1]);
+            }
+        }
+
+        for (int i = 0; i < length; i++) {
+            if (nums[i] > 0) {
+                return i + 1;
+            }
+        }
+
+        return length + 1;
     }
 }
