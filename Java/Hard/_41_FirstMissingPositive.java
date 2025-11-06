e2d9da3b30d66010934ca621eb2e6a7a4f9bab9a
Carlos Hernandez <carlosandresh925@gmail.com>
2025-11-05 18:57:03 -0800

    Add claude.md file

---
+++ Java/Hard/_41_FirstMissingPositive.java
@@ -0,0 +1,18 @@
+package Java.Hard;
+
+public class _41_FirstMissingPositive {
+    public static void main(String[] args) {
+        System.out.println("#41 - First Missing Positive - Hard");
+
+        int[] input = {7, 8, 9, 11, 12};
+        int first = firstMissingPositive(input);
+
+        System.out.println("First Missing Positive: " + first);
+
+        return;
+    }
+
+    private static int firstMissingPositive(int[] nums) {
+        
+    }
+}
