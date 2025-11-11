d7ae7d63754e8c5a3fea9ef482157062e934c8de
Carlos Hernandez <carlosandresh925@gmail.com>
2025-11-10 17:05:55 -0800

    LeetCode 15 - 3sum

---
+++ Java/Medium/_15_ThreeSum.java
@@ -0,0 +1,60 @@
+package Java.Medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class _15_ThreeSum {
+    public static void main(String[] args) {
+        System.out.println("#15 - 3Sum - Medium");
+
+        int[] input = {-1, 0, 1, 2, -1, 4};
+        List<List<Integer>> result = threeSum(input);
+
+        System.out.println("Result: " + result.toString());
+
+        return;
+    }
+
+    private static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+            int target = -nums[i];
+
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+
+                if (sum == target) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+
+                    left++;
+                    right--;
+                }
+                else if (sum < target) {
+                    left++;
+                }
+                else {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+}
