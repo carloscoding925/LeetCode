cbf2494c928487900f8e84a2f5fd073c3e993063
Carlos Hernandez <carlosandresh925@gmail.com>
2025-11-10 16:18:54 -0800

    LeetCode 128 - Longest Consecutive Sequence

---
+++ Java/Medium/_128_LongestConsecSequence.java
@@ -0,0 +1,45 @@
+package Java.Medium;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class _128_LongestConsecSequence {
+    public static void main(String[] args) {
+        System.out.println("#128 - Longest Consecutive Sequence - Medium");
+
+        int[] input = {100, 4, 200, 1, 3, 2};
+        int lcs = longestConsecutive(input);
+
+        System.out.println("Longest Consecutive Sequence: " + lcs);
+
+        return;
+    }
+
+    private static int longestConsecutive(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        Set<Integer> numSet = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            numSet.add(nums[i]);
+        }
+
+        int maxLength = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (!numSet.contains(nums[j] - 1)) {
+                int currentNum = nums[j];
+                int currentLength = 1;
+
+                while (numSet.contains(currentNum + 1)) {
+                    currentNum++;
+                    currentLength++;
+                }
+
+                maxLength = Math.max(maxLength, currentLength);
+            }
+        }
+
+        return maxLength;
+    }
+}
