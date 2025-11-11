320786bedfd8cdc723b24320d5df5f861df4d81e
Carlos Hernandez <carlosandresh925@gmail.com>
2025-11-10 16:05:16 -0800

    LeetCode 238 - Product of Array

---
+++ Java/Medium/_238_ProductOfArray.java
@@ -0,0 +1,34 @@
+package Java.Medium;
+
+import java.util.Arrays;
+
+public class _238_ProductOfArray {
+    public static void main(String[] args) {
+        System.out.println("#238 - Product of Array Except Self - Medium");
+
+        int[] nums = {1, 2, 3, 4};
+        int[] result = productExceptSelf(nums);
+
+        System.out.println("Result: " + Arrays.toString(result));
+
+        return;
+    }
+
+    private static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] answer = new int[n];
+
+        answer[0] = 1;
+        for (int i = 1; i < n; i++) {
+            answer[i] = answer[i - 1] * nums[i - 1];
+        }
+
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            answer[i] = answer[i] * right;
+            right = right * nums[i];
+        }
+
+        return answer;
+    }
+}
