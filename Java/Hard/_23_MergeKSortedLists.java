8c9cce3b7feee12715799ebe274183f287b9be84
Carlos Hernandez <carlosandresh925@gmail.com>
2025-11-11 18:44:37 -0800

    LeetCode 23 - Merge k sorted lists

---
+++ Java/Hard/_23_MergeKSortedLists.java
@@ -0,0 +1,56 @@
+package Java.Hard;
+
+import java.util.PriorityQueue;
+
+public class _23_MergeKSortedLists {
+    public static void main(String[] args) {
+        System.out.println("#23 - Merge k Sorted Lists - Hard");
+
+        ListNode[] lists = {new ListNode(1, new ListNode(4, new ListNode(5))), new ListNode(1, new ListNode(3, new ListNode(4))), new ListNode(2, new ListNode(6))};
+        ListNode merged = mergeKLists(lists);
+
+        while (merged != null) {
+            System.out.println(merged.val);
+            merged = merged.next;
+        }
+
+        return;
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    private static ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+
+        PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a, b) -> a.val - b.val);
+        for (ListNode head : lists) {
+            if (head != null) {
+                minHeap.offer(head);
+            }
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (!minHeap.isEmpty()) {
+            ListNode minNode = minHeap.poll();
+            current.next = minNode;
+            current = current.next;
+
+            if (minNode.next != null) {
+                minHeap.offer(minNode.next);
+            }
+        }
+
+        return dummy.next;
+    }
+}
