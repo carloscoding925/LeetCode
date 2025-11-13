4a29d4f012d9afb3e6a46dd0eadb7fe01e788fdf
Carlos Hernandez <carlosandresh925@gmail.com>
2025-11-12 19:08:32 -0800

    LeetCode 200 - Number of Islands

---
+++ Java/Medium/_200_NumberOfIslands.java
@@ -0,0 +1,55 @@
+package Java.Medium;
+
+public class _200_NumberOfIslands {
+    public static void main(String[] args) {
+        System.out.println("#200 - Number of Islands - Medium");
+
+        char[][] grid = {
+            {'1', '1', '1', '1', '0'}, 
+            {'1', '1', '0', '1', '0'}, 
+            {'1', '1', '0', '0', '0'},
+            {'0', '0', '0', '0', '0'}
+        };
+
+        int numOfIslands = numIslands(grid);
+        System.out.println("Number of Islands: " + numOfIslands);
+
+        return;
+    }
+
+    private static int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int count = 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private static void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length ||
+            j < 0 || j >= grid[0].length ||
+            grid[i][j] == '0') {
+            return;
+        }
+
+        grid[i][j] = '0';
+
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
+    }
+}
