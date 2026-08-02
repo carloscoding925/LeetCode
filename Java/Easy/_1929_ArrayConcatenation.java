31d911ac6f75eeaf8c3adda690aa214824c1415a
Carlos Hernandez <carlosandresh925@gmail.com>
2026-08-01 18:47:23 -0700

    1929

--- Java/Easy/_1929_ArrayConcatenation.java
+++ Java/Easy/_1929_ArrayConcatenation.java
@@ -1,28 +1,29 @@
+// Attempts (2)
+// Last Attempted - August 1st, 2026
+
 package Java.Easy;
+
+import java.util.Arrays;
 
 public class _1929_ArrayConcatenation {
     public static void main(String[] args) {
         System.out.println("#1929 - Concatenation of Array - Easy");
 
-        int[] numsArray = {1, 2, 3};
-        int[] result = getConcatenation(numsArray);
+        int[] input = {1, 2, 1};
+        int[] concatenatedArray = getConcatenation(input);
 
-        for (int i = 0; i < result.length; i++) {
-            System.out.println(result[i]);
-        }
-
-        return;
+        System.out.println("Concatenated Array: " + Arrays.toString(concatenatedArray));
     }
 
     private static int[] getConcatenation(int[] nums) {
-        int length = nums.length;
-        int[] concatenated = new int[length * 2];
+        int size = nums.length;
+        int[] ans = new int[size * 2];
 
-        for (int i = 0; i < length; i++) {
-            concatenated[i] = nums[i];
-            concatenated[length + i] = nums[i];
+        for (int i = 0; i < size; i++) {
+            ans[i] = nums[i];
+            ans[size + i] = nums[i];
         }
 
-        return concatenated;
+        return ans;
     }
 }
